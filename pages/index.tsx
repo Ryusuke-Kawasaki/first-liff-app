2da68d9baa8301ee2437e9187228844d1ec87bf8
Ryusuke-Kawasaki <kawasaki@codable.co.jp>
2022-07-22 15:53:55 +0900

    タイトル変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
   return (
     <div>
       <Head>
-        <title>LIFF App</title>
+        <title>デジトラLineApp</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
